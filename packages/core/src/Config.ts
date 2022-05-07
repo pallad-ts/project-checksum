54d3fef18caef11389d2514125aaf1cbadd626e3
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2022-05-07 12:37:30 +0200

    Savepoint

--- packages/core/src/Config.ts
+++ packages/core/src/Config.ts
@@ -1,13 +1,14 @@
 import {Algorithm} from "./Algorithm";
-import {Validator} from "jsonschema";
-
 
 export interface RawConfig {
 	readonly algorithm: Algorithm;
 	readonly paths?: string[]
+	readonly external?: RawConfig.External[];
 }
 
 export namespace RawConfig {
+	export type External = string | { loader: string, options: unknown };
+
 	export interface Enchanted extends RawConfig {
 		readonly directory: string;
 	}
